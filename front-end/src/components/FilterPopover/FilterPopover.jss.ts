54ca240fc09deb60405c004924c3c96e54b2fdd0
ThiagoSzz <thiagosantosrs2@hotmail.com>
2024-01-11 23:50:06 -0300

    created filter popover

--- front-end/src/components/FilterPopover/FilterPopover.jss.ts
+++ front-end/src/components/FilterPopover/FilterPopover.jss.ts
@@ -3,7 +3,55 @@ import { createUseStyles } from 'react-jss';
 
 type JSSStyles = { [keys: string]: React.CSSProperties | JSSStyles };
 
-const styles: JSSStyles = {};
+const styles: JSSStyles = {
+  popover: {
+    width: '300px'
+  },
+  listItem: {
+    width: '100%',
+    marginBottom: '5px',
+    border: 'solid 1px',
+    borderRadius: '8px',
+    borderColor: '#CAD2D8'
+  },
+  lineFlexBox: {
+    alignItems: 'center'
+  },
+  input: {
+    width: '215px',
+    height: '44px',
+    marginBottom: '5px'
+  },
+  dietaryPreferenceSelect: {
+    width: '215px',
+    height: '44px',
+    marginBottom: '5px'
+  },
+  mealPeriodSelect: {
+    width: '215px',
+    height: '44px',
+    marginBottom: '5px'
+  },
+  footer: {
+    gap: '10px',
+    margin: '7px',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  button: {
+    height: '30px'
+  },
+  closeButton: {
+    height: '30px',
+    marginBottom: '5px',
+    marginLeft: '10px'
+  },
+  searchBarIcon: {
+    alignItems: 'center',
+    alignSelf: 'center'
+  }
+};
 
 export const useStyles = createUseStyles(styles, {
   name: 'FilterPopover'
--- front-end/src/components/FilterPopover/FilterPopover.tsx
+++ front-end/src/components/FilterPopover/FilterPopover.tsx
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-eval */
 import {
+  Button,
+  ButtonDesign,
+  FlexBox,
+  Input,
   List,
   PopoverPlacementType,
   ResponsivePopover,
-  StandardListItem
+  Select,
+  StandardListItem,
+  Option
 } from '@ui5/webcomponents-react';
-import { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { useStyles } from './FilterPopover.jss';
 
 interface FilterPopoverProps {
   isPopoverOpen: boolean;
@@ -14,30 +23,121 @@ interface FilterPopoverProps {
 export const FilterPopover = (props: FilterPopoverProps) => {
   const { isPopoverOpen, setIsPopoverOpen } = props;
 
+  const classes = useStyles();
+  const [isCourseNameFilterActive, setisCourseNameFilterActive] = useState<boolean>(false);
+  const [isDietaryPreferenceFilterActive, setisDietaryPreferenceFilterActive] =
+    useState<boolean>(false);
+  const [isMealPeriodFilterActive, setisMealPeriodFilterActive] = useState<boolean>(false);
+
+  const filterOptions = [
+    {
+      activeState: 'isCourseNameFilterActive',
+      label: 'Nome do curso',
+      placeholder: 'Nome do curso'
+    },
+    {
+      activeState: 'isDietaryPreferenceFilterActive',
+      label: 'Preferência alimentar',
+      placeholder: 'Preferência alimentar'
+    },
+    {
+      activeState: 'isMealPeriodFilterActive',
+      label: 'Turno da refeição',
+      placeholder: 'Turno da refeição'
+    }
+  ];
+
+  const handlePopoverClose = () => {
+    setIsPopoverOpen(false);
+
+    filterOptions.forEach((option) => {
+      option.activeState && eval(`set${option.activeState}(false)`);
+    });
+  };
+
+  const handleFilterInputToggle = (option: any) => {
+    const { activeState, label, placeholder } = option;
+
+    if (eval(activeState)) {
+      eval(`set${activeState}(false)`);
+    } else {
+      eval(`set${activeState}(true)`);
+    }
+  };
+
   return (
     <ResponsivePopover
       opener={'openPopoverBtn'}
       open={isPopoverOpen}
-      onAfterClose={() => setIsPopoverOpen(false)}
-      placementType={PopoverPlacementType.Bottom}
-      headerText="Filtrar por"
+      className={classes.popover}
+      onAfterClose={handlePopoverClose}
+      placementType={PopoverPlacementType.Right}
+      headerText={'Filtrar por'}
+      footer={
+        <FlexBox className={classes.footer}>
+          <Button design={ButtonDesign.Emphasized} className={classes.button}>
+            Aplicar
+          </Button>
+          <Button onClick={() => setIsPopoverOpen(false)} className={classes.button}>
+            Cancelar
+          </Button>
+        </FlexBox>
+      }
     >
       <List>
-        <StandardListItem icon="navigation-right-arrow" iconEnd>
-          Universidade
-        </StandardListItem>
-        <StandardListItem icon="navigation-right-arrow" iconEnd>
-          Código do RU
-        </StandardListItem>
-        <StandardListItem icon="navigation-right-arrow" iconEnd>
-          Curso
-        </StandardListItem>
-        <StandardListItem icon="navigation-right-arrow" iconEnd>
-          Preferência alimentar &nbsp;&nbsp;&nbsp;
-        </StandardListItem>
-        <StandardListItem icon="navigation-right-arrow" iconEnd>
-          Turno da refeição
-        </StandardListItem>
+        {filterOptions.map((option) => (
+          <React.Fragment key={option.label}>
+            <FlexBox>
+              {!eval(option.activeState) ? (
+                <StandardListItem
+                  icon="navigation-right-arrow"
+                  iconEnd
+                  onClick={() => handleFilterInputToggle(option)}
+                  className={classes.listItem}
+                >
+                  {option.label}
+                </StandardListItem>
+              ) : option.activeState === 'isCourseNameFilterActive' ? (
+                <FlexBox className={classes.lineFlexBox}>
+                  <Input className={classes.input} placeholder={option.placeholder} />
+                  <Button
+                    icon="decline"
+                    className={classes.closeButton}
+                    onClick={() => handleFilterInputToggle(option)}
+                  />
+                </FlexBox>
+              ) : option.activeState === 'isDietaryPreferenceFilterActive' ? (
+                <FlexBox className={classes.lineFlexBox}>
+                  <Select className={classes.dietaryPreferenceSelect}>
+                    <Option data-id="Select">Preferência alimentar</Option>
+                    <Option data-id="Onívora">Onívora</Option>
+                    <Option data-id="Vegetariana">Vegetariana</Option>
+                    <Option data-id="Vegana">Vegana</Option>
+                  </Select>
+                  <Button
+                    icon="decline"
+                    className={classes.closeButton}
+                    onClick={() => handleFilterInputToggle(option)}
+                  />
+                </FlexBox>
+              ) : (
+                <FlexBox className={classes.lineFlexBox}>
+                  <Select className={classes.mealPeriodSelect}>
+                    <Option data-id="Select">Turno da refeição</Option>
+                    <Option data-id="Café da manhã">Café da manhã</Option>
+                    <Option data-id="Almoço">Almoço</Option>
+                    <Option data-id="Jantar">Jantar</Option>
+                  </Select>
+                  <Button
+                    icon="decline"
+                    className={classes.closeButton}
+                    onClick={() => handleFilterInputToggle(option)}
+                  />
+                </FlexBox>
+              )}
+            </FlexBox>
+          </React.Fragment>
+        ))}
       </List>
     </ResponsivePopover>
   );
