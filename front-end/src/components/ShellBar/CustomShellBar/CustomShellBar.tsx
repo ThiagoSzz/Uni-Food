4f23f05180946c36883e078082e63e3a58f84d87
ThiagoSzz <thiagosantosrs2@hotmail.com>
2024-02-10 18:47:07 -0300

    added tooltip on shellbar buttons

--- front-end/src/components/ShellBar/CustomShellBar/CustomShellBar.tsx
+++ front-end/src/components/ShellBar/CustomShellBar/CustomShellBar.tsx
@@ -10,6 +10,7 @@ import { ShellBar } from '../ShellBar';
 import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../../enums/AppRoutesEnum';
 import { CustomShellBarProps } from '../../../interfaces/props/CustomShellBarProps';
+import { Tooltip } from 'react-tippy';
 
 export const CustomShellBar = (props: CustomShellBarProps) => {
   const { searchDisabled, createReviewDisabled } = props;
@@ -29,19 +30,27 @@ export const CustomShellBar = (props: CustomShellBarProps) => {
       image="https://static-00.iconduck.com/assets.00/fork-and-knife-with-plate-emoji-2048x2048-4e58vsav.png"
       text="UniFood"
     >
-      <Button
-        design={ButtonDesign.Emphasized}
-        icon="search"
-        disabled={searchDisabled}
-        onClick={() => navigateToHomePage()}
-      ></Button>
-      <Button
-        design={ButtonDesign.Emphasized}
-        icon="add"
-        disabled={createReviewDisabled}
-        onClick={() => navigateToCreateReviewPage()}
-      ></Button>
-      <Button design={ButtonDesign.Default} disabled icon="visits"></Button>
+      <Tooltip title="Pesquisar avaliações" arrow arrowSize="small">
+        <Button
+          design={ButtonDesign.Emphasized}
+          icon="search"
+          disabled={searchDisabled}
+          onClick={() => navigateToHomePage()}
+          tooltip=" "
+        />
+      </Tooltip>
+      <Tooltip title="Criar nova avaliação" arrow arrowSize="small">
+        <Button
+          design={ButtonDesign.Emphasized}
+          icon="add"
+          disabled={createReviewDisabled}
+          onClick={() => navigateToCreateReviewPage()}
+          tooltip=" "
+        />
+      </Tooltip>
+      <Tooltip title="Entrar" arrow arrowSize="small">
+        <Button design={ButtonDesign.Default} disabled icon="visits" tooltip=" " />
+      </Tooltip>
       <Avatar icon="employee" colorScheme={AvatarColorScheme.Accent6} size={AvatarSize.S}></Avatar>
     </ShellBar>
   );
